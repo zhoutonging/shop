72e66fbb663e71d1944a27faa7276071a74d6358
zhoutong <zhoutonging@126.com>
2019-06-27 09:56:40 +0800

    开启断路器功能

--- shop-web-admin/src/main/java/com/imooc/shopwebadmin/feign/TestFeign.java
+++ shop-web-admin/src/main/java/com/imooc/shopwebadmin/feign/TestFeign.java
@@ -1,9 +1,10 @@
 package com.imooc.shopwebadmin.feign;
 
+import com.imooc.shopwebadmin.feign.impl.TestFeignImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "shop-business")
+@FeignClient(value = "shop-business", fallback = TestFeignImpl.class)
 public interface TestFeign {
 
     @GetMapping("/helloWorld")
