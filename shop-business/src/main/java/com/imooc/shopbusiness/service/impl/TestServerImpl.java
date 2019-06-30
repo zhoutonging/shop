f71779a683519242591a0669ec10bdd5066a4f5b
zhoutong <zhoutonging@126.com>
2019-06-30 08:29:40 +0800

    集成Redis和相应测试

--- shop-business/src/main/java/com/imooc/shopbusiness/service/impl/TestServerImpl.java
+++ shop-business/src/main/java/com/imooc/shopbusiness/service/impl/TestServerImpl.java
@@ -3,6 +3,7 @@ package com.imooc.shopbusiness.service.impl;
 import com.imooc.api.Dto.UserDto;
 import com.imooc.api.server.TestServer;
 import com.imooc.shopbusiness.mapper.UserMapper;
+import com.imooc.shopcommon.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Service
 public class TestServerImpl implements TestServer {
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Autowired
     private UserMapper userMapper;
@@ -21,6 +25,15 @@ public class TestServerImpl implements TestServer {
 
     @Override
     public List<UserDto> findAll() {
+
+        if (redisUtil.exists("userDtoList")) {
+            List<UserDto> userDtoList = (List<UserDto>) redisUtil.get("userDtoList");
+            return userDtoList;
+        }
+
+        List<UserDto> userDtoList = userMapper.findAll();
+
+        redisUtil.set("userDtoList", userDtoList);
         return userMapper.findAll();
     }
 }
